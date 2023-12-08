8eea58d5fca51401eeb30efe531929d7c342a2f2
Kostiantyn Polishko <kostantinpolishko@gmail.com>
2023-12-08 21:56:13 +0200

    2 Modified logic for move abstractly cell throw the field.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
-#include <ctime>
+#include <random>
 #include "Cell.h"
+
+bool isGridFilled(const char grid[][10], const short& row, const short& col)
+{
+	return grid[row][col] != 'x';
+}
+
+bool isNotOutRow(const short& rowSize, const short& row)
+{
+	return row >= 0 && row < rowSize;
+}
+
+bool isNotOutCol(const short& colSize, const short& col)
+{
+	return col >= 0 && col < colSize;
+}
+
 
 int main() 
 {
-	srand(time(NULL));
-
+	bool flag = false;
 	short cellSteps[][2] = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
-	short indexStep{};
+	short sizeSteps = std::size(cellSteps);
+	short step{};
 	const short SIZE = 10;
 	char grid[SIZE][SIZE]{};
 
+	std::mt19937 rand(static_cast<unsigned int>(time(0)));
 	std::shared_ptr<Cell> cell = std::make_shared<Cell>(rand()%10, rand()%10);
 
 	for (size_t i = 0; i < 10; i++)
@@ -29,16 +46,30 @@ int main()
 			std::cout << std::endl;
 		}
 		system("CLS");
+
+		do {
+			flag = false;
+			step = rand() % sizeSteps;
+
+			if (!(step % 2)) 
+			{
+				flag = isGridFilled(grid, cell->getRow() + cellSteps[step][0], cell->getCol() + cellSteps[step][1]) && isNotOutRow(SIZE, cell->getRow() + cellSteps[step][0]);
+			}
+			else 
+			{
+				flag = isGridFilled(grid, cell->getRow() + cellSteps[step][0], cell->getCol() + cellSteps[step][1]) && isNotOutRow(SIZE, cell->getCol() + cellSteps[step][1]);
+			}
+
+		} while (!flag);
+
 		grid[cell->getRow()][cell->getCol()] = '-';
 
-	indexStep = rand() % std::size(cellSteps);
-
-	if (cell->getRow() + cellSteps[indexStep][0] >= 0 && cell->getRow() + cellSteps[indexStep][0] < SIZE)
-		cell->setRow(cellSteps[indexStep][0]);
-
-	if (cell->getCol() + cellSteps[indexStep][1] >= 0 && cell->getCol() + cellSteps[indexStep][1] < SIZE)
-		cell->setCol(cellSteps[indexStep][1]);
-
+		if (!(step % 2)) {
+			cell->setRow(cellSteps[step][0]);
+		}
+		else {
+			cell->setCol(cellSteps[step][1]);
+		}
 	}
 
 	return 0;
